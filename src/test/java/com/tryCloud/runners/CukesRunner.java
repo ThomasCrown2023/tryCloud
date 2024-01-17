36e884d61ced95d2ac166123fbee87634aefa1fc
gokhanalay <140934358+gokhanalay@users.noreply.github.com>
2024-01-16 19:41:53 -0800

    cukes runner updated

--- src/test/java/com/tryCloud/runners/CukesRunner.java
+++ src/test/java/com/tryCloud/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/tryCloud/step_definitions",
         dryRun = false,
-        tags = "@B31G11-83",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
