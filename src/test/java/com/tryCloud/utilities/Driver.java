0a51c1d24124e83e35e020827ccd33909532bdd0
Zain <zainobaide70@gmail.com>
2024-01-16 19:50:12 -0800

    Driver Updated

--- src/test/java/com/tryCloud/utilities/Driver.java
+++ src/test/java/com/tryCloud/utilities/Driver.java
@@ -3,7 +3,7 @@ package com.tryCloud.utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Wait;
+
 
 import java.time.Duration;
 
